46d2878075ee4dd0c607399caeadffe5542e7939
tysen7060-sudo <tysen7060@gmail.com>
2026-07-16 19:17:15 +0530

    delete

--- webstore.config.js
+++
@@ -1,4 +0,0 @@
-window.DEVOURMC_WEBSTORE_CONFIG = {
-  // This key is used client-side by Web3Forms. Do not put SMTP passwords or private API secrets here.
-  web3FormsAccessKey: "a3b61e67-de15-4fd7-9bda-3aeefcd177fa"
-};
